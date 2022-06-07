d25d3936ff1a4f5eb84b8328406bd2dcdd914610
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-06-07 07:39:11 +0000

    再学習

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  get '/top' => 'homes#top'
+  
+  resources :lists
+  
+  # 上記で一括にできる
+  # get 'lists/new'
+  # post 'lists'=>'lists#create'
+  # get 'lists'=>'lists#index'
+  # get 'lists/:id'=>'lists#show', as: 'list'
+  # get 'lists/:id/edit'=>'lists#edit', as: 'edit_list'
+  # patch 'lists/:id'=>'lists#update', as: 'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
